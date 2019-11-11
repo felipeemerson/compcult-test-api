e6aee2325622a83030b6334f503ce3e15cbec382
Felipe Emerson <felipeemersonsb@hotmail.com>
2019-11-11 14:07:42 -0300

    Adiciona rota post /api/tarefas

--- routes/tarefas.js
+++ routes/tarefas.js
@@ -16,12 +16,31 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+    const { error } = validaTarefa(req.body);
 
+    if(error) return res.status(400).send(error.details[0].message);
+
+    let tarefa = new Tarefa({
+        titulo: req.body.titulo,
+        descricao: req.body.descricao,
+        prioridade: req.body.prioridade
+    });
+
+    try {
+        tarefa = await tarefa.save();
+        res.send(tarefa);
+    }
+    catch (ex) {
+        for (field in ex.errors)
+            console.log(ex.errors[field].message);
+    }
 });
 
 function validaTarefa(tarefa) {
     const schema = {
-      titulo: Joi.string().min(3).required()
+      titulo: Joi.string().min(3).required(),
+      descricao: Joi.string(),
+      prioridade: Joi.number()
     };
   
     return Joi.validate(tarefa, schema);
